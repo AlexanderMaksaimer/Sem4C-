a253c2da2c169b86eb94e5b30faef0f7e4b05736
«Aleksander <pavel4uk18@gmail.com>
2022-05-19 20:23:11 +0300

    task2ready1

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+string GetNumber ()
+{
+    return Console.ReadLine();
+}
+
+
+int CountNum(string str)
+{
+    return str.Length;
+}
+
+Console.WriteLine("Введите число: ");
+int countNum = CountNum(GetNumber());
+
+Console.WriteLine(countNum);
+
+
