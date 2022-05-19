cd9e067c4e56782080ec44af8702385fa358503b
«Aleksander <pavel4uk18@gmail.com>
2022-05-19 20:36:35 +0300

    task2ready2

--- task2/Program.cs
+++ task2/Program.cs
@@ -5,11 +5,24 @@ string GetNumber ()
     return Console.ReadLine();
 }
 
-
 int CountNum(string str)
 {
-    return str.Length;
+    int num = int.Parse(str);
+    int count = 0;
+    //int? count;
+
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
 }
+
+//int CountNum(string str)
+//{
+//    return str.Length;
+//}
 
 Console.WriteLine("Введите число: ");
 int countNum = CountNum(GetNumber());
